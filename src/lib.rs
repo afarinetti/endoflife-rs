e95c8976c9ca02276333cc6c93077e8b39bc5e98
Antonio Farinetti <afarinetti@users.noreply.github.com>
2025-01-11 16:56:45 -0600

    Corrected an extraneous space

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@ pub struct Cycle {
 /// ```rust
 /// use endoflife_rs::*;
 ///
-// #[tokio::main]
+//  #[tokio::main]
 /// async fn main() -> Result<(), Box<dyn std::error::Error>> {
 ///     let cycles = get_all_details("python").await?;
 ///     for cycle in cycles {
